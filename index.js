a9692a90dff1b7ce688596256e1b1735809b38d0
pascalrascal <marchetti.14@osu.edu>
2016-12-04 13:48:29 -0500

    Initial Commit
    A nice lill express boilerplate where all the code is kept in "modules".
    Inspired by MEAN stack but not nearly as neat and complete, ideal for
    Hackathons and quick lil one-off things

---
+++ index.js
@@ -0,0 +1,23 @@
+var palaces = require("./modules/palaces");
+var express = require("express");
+var fs = require("fs");
+
+console.log("Hello World");
+
+//Initial express app
+var app = express();
+app.engine('html', require('ejs').renderFile);
+
+//Initial all the modules in the module directory
+var files = fs.readdirSync("./modules/");
+for(var i = 0; i < files.length; i++){
+    if(files[i] != 'module_template'){
+        var module = require('./modules/' + files[i]);
+        module(app);
+    }
+}
+
+
+var port = 3000;
+app.listen(port);
+console.log("Listening on port " + port);
---
+++ modules/module_template/server/routes/index.js
@@ -0,0 +1,6 @@
+module.exports = function (app) {
+
+    app.get('/', function (req, res) {
+        res.render(app.core.dirname + "/public/views/index.html");
+    });
+}
